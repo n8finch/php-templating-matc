0214d34b2669fb87d08d344f7e2fe0874c36d960
Nate Finch <finchproservices@gmail.com>
2019-04-07 23:01:45 -0500

    read is all good

--- router/routes.php
+++ router/routes.php
@@ -1,34 +1,37 @@
 <?php
 require PUBLIC_DIR . '/controllers/GuzzlerController.php';
-echo '<pre>';
-var_dump($_SERVER);
-var_dump($_POST);
-echo '</pre>';
-die();
+// echo '<pre>';
+// var_dump($_SERVER);
+// var_dump($_POST);
+// echo '</pre>';
+// die();
 $loader = new \Twig\Loader\FilesystemLoader('./templates');
 $twig = new \Twig\Environment($loader, array());
+
+$guzzler = new GuzzleController();
 
 
 switch ( $_SERVER['REQUEST_URI'] ) {
     case '/create':
-        $guzzle_response = do_that_guzzle_thang();
-        echo $twig->render('create.twig', array('name' => 'Nate'));
-        break;
-        
+    echo $twig->render('create.twig', array('name' => 'Nate'));
+    break;
+    
     case '/read':
-        echo $twig->render('read.twig', array('name' => 'Nate'));
+        $guzzle_response = ( array_key_exists( 'student-id', $_POST ) ) ? $guzzler->readone( $_POST['student-id'] ) : false;
+        echo $twig->render('read.twig', array( 'students' => $guzzle_response ) );
         break;
         
-    case '/update':
+        case '/update':
         echo $twig->render('update.twig', array('name' => 'Nate'));
         break;
         
     case '/delete':
+        $guzzle_response = ( array_key_exists( 'student-id', $_POST ) ) ? $guzzler->delete( $_POST['student-id'] ) : false;
         echo $twig->render('delete.twig', array('name' => 'Nate'));
     break;
     
     default:
-        $guzzle_response = do_that_guzzle_thang();
+        $guzzle_response = $guzzler->readall();
         echo $twig->render('index.twig', array( 'students' => $guzzle_response ) );
     break;
 }
